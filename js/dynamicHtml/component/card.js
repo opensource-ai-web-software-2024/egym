3bebd2b14a54634b97498204e6c23a89ef90b9ce
reo91004 <reo91004@gmail.com>
2024-03-22 05:13:39 +0900

    add: 1. 상단 토글 카테고리 영역 버튼 생성 2. 생성한 영역 CSS 꾸미기
    fail:
    1. 데이터를 매칭하기 위한 리스트는 만들어놓았지만 아직 적용하지 않음
    2. checked가 되있는걸 확인했지만 radio의 CSS가 먹히지 않음

--- js/dynamicHtml/component/card.js
+++ js/dynamicHtml/component/card.js
@@ -1,200 +1,200 @@
-// 운동 정보 (테스트용) 
+// 운동 정보 (테스트용)
 class ExerciseInfo {
-    constructor(targetMuscleGroup, exerciseType, equipmentRequired, mechanics, forceType, experienceLevel, secondaryMuscles) {
-        this.targetMuscleGroup = targetMuscleGroup;
-        this.exerciseType = exerciseType;
-        this.equipmentRequired = equipmentRequired;
-        this.mechanics = mechanics;
-        this.forceType = forceType;
-        this.experienceLevel = experienceLevel;
-        this.secondaryMuscles = secondaryMuscles;
-    }
+	constructor(targetMuscleGroup, exerciseType, equipmentRequired, mechanics, forceType, experienceLevel, secondaryMuscles) {
+		this.targetMuscleGroup = targetMuscleGroup;
+		this.exerciseType = exerciseType;
+		this.equipmentRequired = equipmentRequired;
+		this.mechanics = mechanics;
+		this.forceType = forceType;
+		this.experienceLevel = experienceLevel;
+		this.secondaryMuscles = secondaryMuscles;
+	}
 }
 
 // 카드 정보 (테스트용)
 class CardInfo {
-    constructor(exerciseName, exerciseInfo, exerciseIntroduction, exerciseProcedure, exerciseTips, thumbnailLink, youtubeLink) {
-        this.exerciseName = exerciseName;
-        this.exerciseInfo = exerciseInfo;
-        this.exerciseIntroduction = exerciseIntroduction;
-        this.exerciseProcedure = exerciseProcedure;
-        this.exerciseTips = exerciseTips;
-        this.thumbnailLink = thumbnailLink;
-        this.youtubeLink = youtubeLink;
-    }
+	constructor(exerciseName, exerciseInfo, exerciseIntroduction, exerciseProcedure, exerciseTips, thumbnailLink, youtubeLink) {
+		this.exerciseName = exerciseName;
+		this.exerciseInfo = exerciseInfo;
+		this.exerciseIntroduction = exerciseIntroduction;
+		this.exerciseProcedure = exerciseProcedure;
+		this.exerciseTips = exerciseTips;
+		this.thumbnailLink = thumbnailLink;
+		this.youtubeLink = youtubeLink;
+	}
 }
-
 
 function filteringHandler() {
-    searchResult = cardInfos;
-    searchResult = searchResult.filter((exerciseInfo) => {
-        return query === exerciseInfo.exerciseName;
-    });
-    console.log(searchResult);
-    makingCards();
+	searchResult = cardInfos;
+	searchResult = searchResult.filter((exerciseInfo) => {
+		return query == exerciseInfo.exerciseInfo.targetMuscleGroup;
+		// return query === exerciseInfo.exerciseName;
+	});
+	console.log(searchResult);
+	makingCards();
 }
 
-
 var cardInfos = [
-    new CardInfo(
-        "복근", 
-        new ExerciseInfo("복근", "힘", "맨몸 운동", "고립 운동", "장력", "초보자", "없음"), 
-        "라잉 플로어 레그 레이즈는 복부 크런치의 변형이며, 복부 근육을 고립시키는 데 사용되는 운동입니다.\n기본적으로 굴곡 기반의 운동으로, 움직이는 동안 하부 복부 근육을 지속적으로 긴장시킵니다",
-        [
-            "편안한 자세로 등을 바닥에 눕습니다. 다리는 직진하고 손은 허리 아래에 두어 지지합니다.",
-            "다리를 직진한 상태로 유지하고 복부를 수축시키고 숨을 내쉬면서 이를 이마 쪽으로 들어올립니다.",
-            "복부가 완전히 수축되고 다리가 약간 평행한 상태가 되면 천천히 다리를 시작 위치로 내려놓습니다.",
-            "할당된 반복 횟수를 완료합니다."
-        ],
-        [
-            "촛불을 끄는 듯이 강하게 숨을 내쉬고, 마음과 근육 사이의 연결을 개선하기 위해 수축을 1초 동안 유지합니다.",
-            "이 운동 중에 허리 아랫부분에 불편함이 느껴진다면, 더 많은 안티 익스텐션 및 안티 로테이션을 기반으로 하는 동작을 선택하세요.",
-            "머리 뒤에 손을 두는 것을 피하세요. 이렇게 하면 목에 과도한 압력이 가해질 수 있습니다."
-        ],
-        "https://i.ytimg.com/vi_webp/r24ntO4IvKc/maxresdefault.webp",
-        "https://youtu.be/r24ntO4IvKc",
-    ),
-    new CardInfo(
-        "종아리", 
-        new ExerciseInfo("종아리", "힘", "맨몸 운동", "고립 운동", "장력", "초보자", "없음"), 
-        "라잉 플로어 레그 레이즈는 복부 크런치의 변형이며, 복부 근육을 고립시키는 데 사용되는 운동입니다.\n기본적으로 굴곡 기반의 운동으로, 움직이는 동안 하부 복부 근육을 지속적으로 긴장시킵니다",
-        [
-            "편안한 자세로 등을 바닥에 눕습니다. 다리는 직진하고 손은 허리 아래에 두어 지지합니다.",
-            "다리를 직진한 상태로 유지하고 복부를 수축시키고 숨을 내쉬면서 이를 이마 쪽으로 들어올립니다.",
-            "복부가 완전히 수축되고 다리가 약간 평행한 상태가 되면 천천히 다리를 시작 위치로 내려놓습니다.",
-            "할당된 반복 횟수를 완료합니다."
-        ],
-        [
-            "촛불을 끄는 듯이 강하게 숨을 내쉬고, 마음과 근육 사이의 연결을 개선하기 위해 수축을 1초 동안 유지합니다.",
-            "이 운동 중에 허리 아랫부분에 불편함이 느껴진다면, 더 많은 안티 익스텐션 및 안티 로테이션을 기반으로 하는 동작을 선택하세요.",
-            "머리 뒤에 손을 두는 것을 피하세요. 이렇게 하면 목에 과도한 압력이 가해질 수 있습니다."
-        ],
-        "https://i.ytimg.com/vi_webp/r24ntO4IvKc/maxresdefault.webp",
-        "https://youtu.be/r24ntO4IvKc",
-    ),
-    new CardInfo(
-        "어깨", 
-        new ExerciseInfo("어깨", "힘", "맨몸 운동", "고립 운동", "장력", "초보자", "없음"), 
-        "라잉 플로어 레그 레이즈는 복부 크런치의 변형이며, 복부 근육을 고립시키는 데 사용되는 운동입니다.\n기본적으로 굴곡 기반의 운동으로, 움직이는 동안 하부 복부 근육을 지속적으로 긴장시킵니다",
-        [
-            "편안한 자세로 등을 바닥에 눕습니다. 다리는 직진하고 손은 허리 아래에 두어 지지합니다.",
-            "다리를 직진한 상태로 유지하고 복부를 수축시키고 숨을 내쉬면서 이를 이마 쪽으로 들어올립니다.",
-            "복부가 완전히 수축되고 다리가 약간 평행한 상태가 되면 천천히 다리를 시작 위치로 내려놓습니다.",
-            "할당된 반복 횟수를 완료합니다."
-        ],
-        [
-            "촛불을 끄는 듯이 강하게 숨을 내쉬고, 마음과 근육 사이의 연결을 개선하기 위해 수축을 1초 동안 유지합니다.",
-            "이 운동 중에 허리 아랫부분에 불편함이 느껴진다면, 더 많은 안티 익스텐션 및 안티 로테이션을 기반으로 하는 동작을 선택하세요.",
-            "머리 뒤에 손을 두는 것을 피하세요. 이렇게 하면 목에 과도한 압력이 가해질 수 있습니다."
-        ],
-        "https://i.ytimg.com/vi_webp/r24ntO4IvKc/maxresdefault.webp",
-        "https://youtu.be/r24ntO4IvKc",
-    ),
-]
+	new CardInfo(
+		"라잉 플로어 레그 레이즈",
+		new ExerciseInfo("복근", "힘", "맨몸 운동", "고립 운동", "장력", "초보자", "없음"),
+		"라잉 플로어 레그 레이즈는 복부 크런치의 변형이며, 복부 근육을 고립시키는 데 사용되는 운동입니다.\n기본적으로 굴곡 기반의 운동으로, 움직이는 동안 하부 복부 근육을 지속적으로 긴장시킵니다",
+		[
+			"편안한 자세로 등을 바닥에 눕습니다. 다리는 직진하고 손은 허리 아래에 두어 지지합니다.",
+			"다리를 직진한 상태로 유지하고 복부를 수축시키고 숨을 내쉬면서 이를 이마 쪽으로 들어올립니다.",
+			"복부가 완전히 수축되고 다리가 약간 평행한 상태가 되면 천천히 다리를 시작 위치로 내려놓습니다.",
+			"할당된 반복 횟수를 완료합니다.",
+		],
+		[
+			"촛불을 끄는 듯이 강하게 숨을 내쉬고, 마음과 근육 사이의 연결을 개선하기 위해 수축을 1초 동안 유지합니다.",
+			"이 운동 중에 허리 아랫부분에 불편함이 느껴진다면, 더 많은 안티 익스텐션 및 안티 로테이션을 기반으로 하는 동작을 선택하세요.",
+			"머리 뒤에 손을 두는 것을 피하세요. 이렇게 하면 목에 과도한 압력이 가해질 수 있습니다.",
+		],
+		"https://i.ytimg.com/vi_webp/r24ntO4IvKc/maxresdefault.webp",
+		"https://youtu.be/r24ntO4IvKc"
+	),
+	new CardInfo(
+		"푸쉬 업",
+		new ExerciseInfo("종아리", "힘", "맨몸 운동", "고립 운동", "장력", "초보자", "없음"),
+		"라잉 플로어 레그 레이즈는 복부 크런치의 변형이며, 복부 근육을 고립시키는 데 사용되는 운동입니다.\n기본적으로 굴곡 기반의 운동으로, 움직이는 동안 하부 복부 근육을 지속적으로 긴장시킵니다",
+		[
+			"편안한 자세로 등을 바닥에 눕습니다. 다리는 직진하고 손은 허리 아래에 두어 지지합니다.",
+			"다리를 직진한 상태로 유지하고 복부를 수축시키고 숨을 내쉬면서 이를 이마 쪽으로 들어올립니다.",
+			"복부가 완전히 수축되고 다리가 약간 평행한 상태가 되면 천천히 다리를 시작 위치로 내려놓습니다.",
+			"할당된 반복 횟수를 완료합니다.",
+		],
+		[
+			"촛불을 끄는 듯이 강하게 숨을 내쉬고, 마음과 근육 사이의 연결을 개선하기 위해 수축을 1초 동안 유지합니다.",
+			"이 운동 중에 허리 아랫부분에 불편함이 느껴진다면, 더 많은 안티 익스텐션 및 안티 로테이션을 기반으로 하는 동작을 선택하세요.",
+			"머리 뒤에 손을 두는 것을 피하세요. 이렇게 하면 목에 과도한 압력이 가해질 수 있습니다.",
+		],
+		"https://i.ytimg.com/vi_webp/r24ntO4IvKc/maxresdefault.webp",
+		"https://youtu.be/r24ntO4IvKc"
+	),
+	new CardInfo(
+		"레그 익스텐션",
+		new ExerciseInfo("어깨", "힘", "맨몸 운동", "고립 운동", "장력", "초보자", "없음"),
+		"라잉 플로어 레그 레이즈는 복부 크런치의 변형이며, 복부 근육을 고립시키는 데 사용되는 운동입니다.\n기본적으로 굴곡 기반의 운동으로, 움직이는 동안 하부 복부 근육을 지속적으로 긴장시킵니다",
+		[
+			"편안한 자세로 등을 바닥에 눕습니다. 다리는 직진하고 손은 허리 아래에 두어 지지합니다.",
+			"다리를 직진한 상태로 유지하고 복부를 수축시키고 숨을 내쉬면서 이를 이마 쪽으로 들어올립니다.",
+			"복부가 완전히 수축되고 다리가 약간 평행한 상태가 되면 천천히 다리를 시작 위치로 내려놓습니다.",
+			"할당된 반복 횟수를 완료합니다.",
+		],
+		[
+			"촛불을 끄는 듯이 강하게 숨을 내쉬고, 마음과 근육 사이의 연결을 개선하기 위해 수축을 1초 동안 유지합니다.",
+			"이 운동 중에 허리 아랫부분에 불편함이 느껴진다면, 더 많은 안티 익스텐션 및 안티 로테이션을 기반으로 하는 동작을 선택하세요.",
+			"머리 뒤에 손을 두는 것을 피하세요. 이렇게 하면 목에 과도한 압력이 가해질 수 있습니다.",
+		],
+		"https://i.ytimg.com/vi_webp/r24ntO4IvKc/maxresdefault.webp",
+		"https://youtu.be/r24ntO4IvKc"
+	),
+];
 
-for (let i = 0; i < 1000; i++) { 
-    cardInfos.push(new CardInfo(
-        "복근", 
-        new ExerciseInfo("복근", "힘", "맨몸 운동", "고립 운동", "장력", "초보자", "없음"), 
-        "라잉 플로어 레그 레이즈는 복부 크런치의 변형이며, 복부 근육을 고립시키는 데 사용되는 운동입니다.\n기본적으로 굴곡 기반의 운동으로, 움직이는 동안 하부 복부 근육을 지속적으로 긴장시킵니다",
-        [
-            "편안한 자세로 등을 바닥에 눕습니다. 다리는 직진하고 손은 허리 아래에 두어 지지합니다.",
-            "다리를 직진한 상태로 유지하고 복부를 수축시키고 숨을 내쉬면서 이를 이마 쪽으로 들어올립니다.",
-            "복부가 완전히 수축되고 다리가 약간 평행한 상태가 되면 천천히 다리를 시작 위치로 내려놓습니다.",
-            "할당된 반복 횟수를 완료합니다."
-        ],
-        [
-            "촛불을 끄는 듯이 강하게 숨을 내쉬고, 마음과 근육 사이의 연결을 개선하기 위해 수축을 1초 동안 유지합니다.",
-            "이 운동 중에 허리 아랫부분에 불편함이 느껴진다면, 더 많은 안티 익스텐션 및 안티 로테이션을 기반으로 하는 동작을 선택하세요.",
-            "머리 뒤에 손을 두는 것을 피하세요. 이렇게 하면 목에 과도한 압력이 가해질 수 있습니다."
-        ],
-        "https://i.ytimg.com/vi_webp/r24ntO4IvKc/maxresdefault.webp",
-        "https://youtu.be/r24ntO4IvKc",
-    ));
+for (let i = 0; i < 1000; i++) {
+	cardInfos.push(
+		new CardInfo(
+			"복근",
+			new ExerciseInfo("복근", "힘", "맨몸 운동", "고립 운동", "장력", "초보자", "없음"),
+			"라잉 플로어 레그 레이즈는 복부 크런치의 변형이며, 복부 근육을 고립시키는 데 사용되는 운동입니다.\n기본적으로 굴곡 기반의 운동으로, 움직이는 동안 하부 복부 근육을 지속적으로 긴장시킵니다",
+			[
+				"편안한 자세로 등을 바닥에 눕습니다. 다리는 직진하고 손은 허리 아래에 두어 지지합니다.",
+				"다리를 직진한 상태로 유지하고 복부를 수축시키고 숨을 내쉬면서 이를 이마 쪽으로 들어올립니다.",
+				"복부가 완전히 수축되고 다리가 약간 평행한 상태가 되면 천천히 다리를 시작 위치로 내려놓습니다.",
+				"할당된 반복 횟수를 완료합니다.",
+			],
+			[
+				"촛불을 끄는 듯이 강하게 숨을 내쉬고, 마음과 근육 사이의 연결을 개선하기 위해 수축을 1초 동안 유지합니다.",
+				"이 운동 중에 허리 아랫부분에 불편함이 느껴진다면, 더 많은 안티 익스텐션 및 안티 로테이션을 기반으로 하는 동작을 선택하세요.",
+				"머리 뒤에 손을 두는 것을 피하세요. 이렇게 하면 목에 과도한 압력이 가해질 수 있습니다.",
+			],
+			"https://i.ytimg.com/vi_webp/r24ntO4IvKc/maxresdefault.webp",
+			"https://youtu.be/r24ntO4IvKc"
+		)
+	);
 }
 var searchResult = cardInfos;
 
 function makingCards() {
-    // div.card 검색  
-    // 유사 배열 객체
-    var searchResultDiv = document.getElementById("search-result");
-    searchResultDiv.innerHTML = "";
-    searchResult.forEach((searchInfo) => {
-        // 카드 추가
-        var card = document.createElement("div"); 
-        card.classList.add("card");
-        searchResultDiv.appendChild(card);
+	// div.card 검색
+	// 유사 배열 객체
+	var searchResultDiv = document.getElementById("search-result");
+	searchResultDiv.innerHTML = "";
+	searchResult.forEach((searchInfo) => {
+		// 카드 추가
+		var card = document.createElement("div");
+		card.classList.add("card");
+		searchResultDiv.appendChild(card);
 
-        // 카드 div 추가
-        var cardDiv = document.createElement("div");
-        cardDiv.classList.add("card-div");
-        card.appendChild(cardDiv);
-        console.log(cardDiv);
+		// 카드 div 추가
+		var cardDiv = document.createElement("div");
+		cardDiv.classList.add("card-div");
+		card.appendChild(cardDiv);
 
-        // 카드 이미지 추가
-        var cardImg = document.createElement("div");
-        cardImg.classList.add("card-img");
-        cardDiv.appendChild(cardImg);
-        
-        // 카드 이미지 삽입
-        var thumbnail = document.createElement("img");
-        thumbnail.classList.add("thumbnail");
-        thumbnail.src = searchInfo.thumbnailLink;
-        cardImg.appendChild(thumbnail);
+		// 카드 이미지 추가
+		var cardImg = document.createElement("div");
+		cardImg.classList.add("card-img");
+		cardDiv.appendChild(cardImg);
 
-        // 카드 푸터 추가
-        var cardFooter = document.createElement("div");
-        cardFooter.classList.add("card-footer");
-        cardDiv.appendChild(cardFooter);
+		// 카드 이미지 삽입
+		var thumbnail = document.createElement("img");
+		thumbnail.classList.add("thumbnail");
+		thumbnail.src = searchInfo.thumbnailLink;
+		cardImg.appendChild(thumbnail);
 
-        // 푸터에 이름 삽입
-        var cardName = document.createElement("h3");
-        cardName.classList.add("card-name");
-        cardName.textContent = searchInfo.exerciseName;
-        cardFooter.appendChild(cardName);
+		// 카드 푸터 추가
+		var cardFooter = document.createElement("div");
+		cardFooter.classList.add("card-footer");
+		cardDiv.appendChild(cardFooter);
 
-        // 푸터에 카드 마크div 추가
-        var cardMarks = document.createElement("div");
-        cardMarks.classList.add("card-marks");
-        cardFooter.appendChild(cardMarks);
+		// 푸터에 이름 삽입
+		var cardName = document.createElement("h3");
+		cardName.classList.add("card-name");
+		cardName.textContent = searchInfo.exerciseName;
+		cardFooter.appendChild(cardName);
 
-        var markInfos = [
-            {
-                key: "자극 부위",
-                value: searchInfo.exerciseInfo.targetMuscleGroup,
-            },
-            {
-                key: "운동 장비",
-                value: searchInfo.exerciseInfo.equipmentRequired,
-            },
-            {
-                key: "운동 구분",
-                value: searchInfo.exerciseInfo.mechanics,
-            },
-            {
-                key: "난이도",
-                value: searchInfo.exerciseInfo.experienceLevel,
-            },
-        ]
+		// 푸터에 카드 마크div 추가
+		var cardMarks = document.createElement("div");
+		cardMarks.classList.add("card-marks");
+		cardFooter.appendChild(cardMarks);
 
-        markInfos.forEach((markInfo) => {
-            // 카드 마크 추가
-            var cardMark = document.createElement("div");
-            cardMark.classList.add("card-mark");
+		var markInfos = [
+			{
+				key: "자극 부위",
+				value: searchInfo.exerciseInfo.targetMuscleGroup,
+			},
+			{
+				key: "운동 장비",
+				value: searchInfo.exerciseInfo.equipmentRequired,
+			},
+			{
+				key: "운동 구분",
+				value: searchInfo.exerciseInfo.mechanics,
+			},
+			{
+				key: "난이도",
+				value: searchInfo.exerciseInfo.experienceLevel,
+			},
+		];
 
-            cardMarks.appendChild(cardMark);
-            // 카드 마크에 key와 value 추가
-            var key = document.createElement("p");
-            key.classList.add("key");
-            key.textContent = markInfo.key;
-            cardMark.appendChild(key);
+		markInfos.forEach((markInfo) => {
+			// 카드 마크 추가
+			var cardMark = document.createElement("div");
+			cardMark.classList.add("card-mark");
 
-            var value = document.createElement("p");
-            value.classList.add("value");
-            value.textContent = markInfo.value;
-            cardMark.appendChild(value);
-        }) 
-    });
+			cardMarks.appendChild(cardMark);
+			// 카드 마크에 key와 value 추가
+			var key = document.createElement("p");
+			key.classList.add("key");
+			key.textContent = markInfo.key;
+			cardMark.appendChild(key);
+
+			var value = document.createElement("p");
+			value.classList.add("value");
+			value.textContent = markInfo.value;
+			cardMark.appendChild(value);
+		});
+	});
 }
 
 makingCards();
--- js/dynamicHtml/component/radioButton.js
+++ js/dynamicHtml/component/radioButton.js
@@ -1,101 +1,125 @@
-{/* <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                        <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-                        <label class="btn btn-outline-primary" for="btnradio1">Radio 1</label>
-                      
-                        <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-                        <label class="btn btn-outline-primary" for="btnradio2">Radio 2</label>
-                      
-                        <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-                        <label class="btn btn-outline-primary" for="btnradio3">Radio 3</label>
-                    </div> */}
+// 추후 데이터베이스 연결을 위한
+const muscleTranslation = {
+	대퇴근: "abductors",
+	복근: "abs",
+	대퇴내전근: "adductors",
+	이두근: "biceps",
+	종아리: "calves",
+	가슴: "chest",
+	팔뚝: "forearms",
+	둔근: "glutes",
+	햄스트링: "hamstrings",
+	"고관절 굴곡근": "hip flexors",
+	장경인대: "it band",
+	광배근: "lats",
+	"등 아래쪽": "lower back",
+	"등 위쪽": "upper back",
+	목: "neck",
+	외복사근: "obliques",
+	긴손바닥근: "palmar fascia",
+	족저근막: "plantar fascia",
+	대퇴사두근: "quads",
+	어깨: "shoulders",
+	승모근: "traps",
+	삼두근: "triceps",
+};
 
-// div#category-selectors 검색
-var categorySelectors = document.getElementById("category-selectors");
+const musclePart = {
+	하체: ["대퇴근", "대퇴내전근", "종아리", "둔근", "햄스트링", "고관절 굴곡근", "장경인대", "허벅지", "족저근막"],
+	가슴: ["가슴"],
+	등: ["광배근", "등 아래쪽", "등 위쪽"],
+	팔: ["팔뚝", "긴손바닥근", "어깨"],
+	복부: ["복근"],
+};
 
-    
-const selectedValue = [
-   "전체", "전체", "전체", "전체"
+const categoryInfos = [
+	{
+		categoryName: "부위",
+		categoryTag: "part",
+		toggleBtnList: ["전체", "하체", "가슴", "등", "팔", "복부", "기타"],
+	},
+	{
+		categoryName: "상세 부위",
+		categoryTag: "detail-part",
+		toggleBtnList: [
+			"전체",
+			"대퇴근",
+			"복근",
+			"대퇴내전근",
+			"이두근",
+			"종아리",
+			"가슴",
+			"팔뚝",
+			"둔근",
+			"햄스트링",
+			"고관절 굴곡근",
+			"장경인대",
+			"광배근",
+			"등 아래쪽",
+			"등 위쪽",
+			"목",
+			"외복사근",
+			"긴손바닥근",
+			"족저근막",
+			"대퇴사두근",
+			"어깨",
+			"승모근",
+			"삼두근",
+		],
+	},
+	{
+		categoryName: "운동 구분",
+		categoryTag: "exercise-part",
+		toggleBtnList: ["전체", "맨몸 운동", "기구 운동"],
+	},
+	{
+		categoryName: "난이도",
+		categoryTag: "difficulty",
+		toggleBtnList: ["전체", "초급", "중급", "고급"],
+	},
 ];
 
-var categorySelectorInfos = [
-    {
-        krName: "부위",
-        name: "part",
-        toggleBtnList: ["전체", "하체", "가슴", "등"],
-    },
-    {
-        krName: "부위",
-        name: "part",
-        toggleBtnList: ["전체", "하체", "가슴", "등"],
-    },
-    {
-        krName: "부위",
-        name: "part",
-        toggleBtnList: ["전체", "하체", "가슴", "등"],
-    },
-    {
-        krName: "부위",
-        name: "part",
-        toggleBtnList: ["전체", "하체", "가슴", "등"],
-    },
-]
+const categorySelectors = document.getElementById("category-selectors");
 
-function toggleHandler(event) {
-    const selectedValue = event.target.value;
-    console.log(selectedValue);
-    return selectedValue;
-}
+categoryInfos.forEach((category) => {
+	// 카테고리 컨테이너 제작
+	const categoryDiv = document.createElement("div");
+	categoryDiv.id = category.categoryTag;
+	categoryDiv.classList.add("category-container"); // CSS 적용을 위한 요소
 
-    
-categorySelectorInfos.forEach((categorySelectorInfo) => {
-    var categorySelectorDiv = document.createElement("div");
-    categorySelectorDiv.classList.add("category-selector-div");
-    categorySelectors.appendChild(categorySelectorDiv);
+	// 제목 추가
+	const categoryTitle = document.createElement("h2");
+	categoryTitle.textContent = category.categoryName;
+	categoryDiv.appendChild(categoryTitle);
 
-    var categoryName = document.createElement("h3");
-    categoryName.classList.add("category-name")
-    categoryName.textContent = categorySelectorInfo.krName;
-    categorySelectorDiv.appendChild(categoryName);
- 
-    categorySelectorInfo.toggleBtnList.forEach((toggleBtn) => {
-        let i = 0;
-        var button = document.createElement("input");
-        button.classList.add("category-selector-button");
-        button.id =  categorySelectorInfo.name + toggleBtn + i;
-        button.type = "radio";
-        button.name = categorySelectorInfo.name;
-        button.value = toggleBtn;
-        categorySelectorDiv.appendChild(button);
+	// 라디오 버튼 넣을 컨테이너 추가
+	const radioContainer = document.createElement("div");
+	radioContainer.classList.add("radio-container"); // 가로 정렬
 
-        button.addEventListener("change", toggleHandler);
-        var label = document.createElement("label");
-        label.classList.add("category-selector-button-label");
-        label.textContent = button.value;
-        label.htmlFor = button.id;
-        categorySelectorDiv.appendChild(label);
-        
-        i++;
-    });
-    // categorySelectorInfo.toggleBtnList.forEach((element) => {
-    //     var button = document.createElement("input");
-    //     button.classList.add("category-selector-button");
-    //     button.id = "radio" + i;
-    //     button.type = "radio";
-    //     button.name = categorySelectorInfo.name; 
-    //     button.value = element;
-    //     if (i === 0) {
-    //         console.log("첫 번째 버튼");
-    //         button.checked = true; // checked 속성 추가
-    //     }
-    //     console.log(button.checked);
-    //     categorySelectorDiv.appendChild(button);
-        
-    //     var label = document.createElement("label");
-    //     label.classList.add("category-selector-button-label");
-    //     label.textContent = button.value;
-    //     label.htmlFor = button.id;
-    //     categorySelectorDiv.appendChild(label);
+	let index = 0;
+	// 라디오 버튼 추가
+	category.toggleBtnList.forEach((element) => {
+		const radioLabel = document.createElement("label");
+		const radioInput = document.createElement("input");
+		radioInput.type = "radio";
+		// 같은 그룹 묶기 위해 categoryTag
+		radioInput.name = category.categoryTag;
+		// value 우선 한글로 저장, 추후 데이터 연결 위해서 영어로 변환 필요성?
+		radioInput.value = element;
+		// 맨 처음 요소는 checked
+		if (index == 0) {
+			index = 1;
+			radioInput.checked = true;
+		}
+		// radio 버튼 label에 삽입
+		radioLabel.appendChild(radioInput);
+		// 라벨 텍스트 지정 (toggleBtnList 항목)
+		radioLabel.appendChild(document.createTextNode(element));
+		radioContainer.appendChild(radioLabel);
+	});
+	categoryDiv.appendChild(radioContainer);
+	index = 0;
 
-    //     i++;
-    // })
-})
+	// 카테고리 선택기에 추가
+	categorySelectors.appendChild(categoryDiv);
+});
