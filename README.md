74f3098ce319f6d7a68e26eecc7dc6156d472d50
reo91004 <reo91004@gmail.com>
2024-03-31 20:07:28 +0900

    add: 루틴 페이지 추가

--- README.md
+++ README.md
@@ -1,11 +1,61 @@
 # 2024-1 오픈소스AI 웹소프트웨어 팀프로젝트
 
 ## Egym
+
 운동 웹 다이어리
 
 ## 상세 기능
 
 ## 팀원
+
 - 김창현
 - 김윤태
 - 박용성
+
+```
+egym_2024
+├─ .DS_Store
+├─ README.md
+├─ __pycache__
+│  ├─ readCsv.cpython-311.pyc
+│  └─ readCsv.cpython-312.pyc
+├─ css
+│  ├─ exercise.css
+│  ├─ routine_style.css
+│  └─ style.css
+├─ csv
+│  └─ test.csv
+├─ favicon.ico
+├─ html
+│  ├─ exercise.html
+│  ├─ index.html
+│  ├─ record_detail.html
+│  ├─ routine.html
+│  └─ test.html
+├─ js
+│  ├─ crawl.js
+│  ├─ csvHandler.js
+│  ├─ dynamicHtml
+│  │  ├─ basicSetting.js
+│  │  ├─ class
+│  │  │  ├─ cardInfo.js
+│  │  │  └─ exerciseInfo.js
+│  │  └─ component
+│  │     ├─ card.js
+│  │     ├─ categoryClassification.js
+│  │     ├─ createRoutineList.js
+│  │     ├─ exerciseInfoContainer.js
+│  │     ├─ header.js
+│  │     ├─ radioButton.js
+│  │     └─ searchBar.js
+│  ├─ key.js
+│  └─ translator.js
+├─ py
+│  ├─ __pycache__
+│  │  └─ readCsv.cpython-312.pyc
+│  ├─ proxyServer.py
+│  └─ readCsv.py
+└─ src
+   └─ img
+
+```
---
+++ js/dynamicHtml/component/createRoutineList.js
@@ -0,0 +1,102 @@
+const routineListSelector = document.getElementById("routine-list");
+
+document.addEventListener("DOMContentLoaded", function () {
+	var createButton = document.getElementById("create");
+
+	createButton.addEventListener("click", function () {
+		// 루틴 제목 자동 생성을 위한 날짜 저장
+		const today = new Date();
+		const year = today.getFullYear(); // 연도
+		const month = today.getMonth() + 1; // 월 (0부터 시작하므로 1을 더해줌)
+		const day = today.getDate(); // 일
+
+		const todayDate = year + "-" + month + "-" + day;
+
+		// new-routine 박스 생성 (루틴 큰 상자)
+		const routineDiv = document.createElement("div");
+		routineDiv.classList.add("new-routine");
+
+		// new-routine 박스 타이틀 설정
+		const routineTitle = document.createElement("h2");
+		routineTitle.textContent = todayDate;
+		routineDiv.appendChild(routineTitle);
+
+		// new-routine 박스 토글 버튼 추가
+		const toggleButton = document.createElement("span");
+		toggleButton.classList.add("toggle-button", "material-icons");
+		toggleButton.textContent = "expand_more";
+		routineDiv.appendChild(toggleButton);
+
+		// new-routine에서 토글 버튼 눌렀을 시 펼쳐질 additional-content
+		const additionalContent = document.createElement("div");
+		additionalContent.classList.add("additional-content");
+
+		// additional-content에 들어갈 삭제 버튼
+		const deletButton = document.createElement("span");
+		const deletButtonLabel = document.createElement("span");
+		deletButtonLabel.classList.add("button-label");
+		deletButtonLabel.textContent = "삭제하기";
+		deletButton.classList.add("material-icons", "delete-button");
+		deletButton.textContent = "delete";
+
+		deletButton.appendChild(deletButtonLabel);
+		additionalContent.appendChild(deletButton);
+
+		routineDiv.appendChild(additionalContent);
+
+		routineListSelector.appendChild(routineDiv);
+	});
+
+	// new-routine 제목 변경 로직
+	routineListSelector.addEventListener("click", function (event) {
+		// 클릭된 요소가 h2인지 확인
+		if (event.target.tagName === "H2") {
+			// h2 요소를 클릭했을 때 처리할 내용
+
+			// 새로 input 박스 생성
+			const currentText = event.target.textContent;
+			const input = document.createElement("input");
+			input.type = "text";
+			input.value = currentText;
+			input.style.width = event.target.offsetWidth + "px";
+			input.style.height = event.target.offsetHeight + "px";
+			input.style.font = window.getComputedStyle(event.target).font;
+
+			// 입력 상자 이벤트
+			input.addEventListener("blur", function () {
+				event.target.textContent = input.value;
+			});
+
+			event.target.textContent = "";
+			event.target.appendChild(input);
+			input.focus();
+		}
+	});
+
+	// 세부 루틴 펼치기, 접기
+	routineListSelector.addEventListener("click", function (event) {
+		if (event.target.classList.contains("toggle-button")) {
+			const routineDiv = event.target.closest(".new-routine");
+			const additionalContent = routineDiv.querySelector(".additional-content");
+
+			if (additionalContent.style.display === "none") {
+				additionalContent.style.display = "flex"; // 수직으로 내용을 나열하도록 함
+				event.target.textContent = "expand_less"; // 아이콘 변경
+			} else {
+				additionalContent.style.display = "none";
+				event.target.textContent = "expand_more"; // 아이콘 변경
+			}
+		}
+	});
+
+	// 루틴 삭제
+	routineListSelector.addEventListener("click", function (event) {
+		if (event.target.classList.contains("delete-button") || event.target.classList.contains("button-label")) {
+			const routineDiv = event.target.closest(".new-routine");
+			routineDiv.classList.add("fade-out"); // 삭제 효과
+			setTimeout(function () {
+				routineDiv.remove(); // 일정 시간 후 요소 삭제
+			}, 300); // 0.3초 후에 삭제
+		}
+	});
+});
