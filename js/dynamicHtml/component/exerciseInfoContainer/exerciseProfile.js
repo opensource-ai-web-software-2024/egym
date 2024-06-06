3dc4b8efc1633ef35f35328ddea39d9b7a7af000
reo91004 <reo91004@gmail.com>
2024-06-06 11:23:41 +0900

    카테고리 기능 강화, Profile 한글전환

--- js/dynamicHtml/component/exerciseInfoContainer/exerciseProfile.js
+++ js/dynamicHtml/component/exerciseInfoContainer/exerciseProfile.js
@@ -1,28 +1,40 @@
+// 한글 출력을 위해 변환
+const keyMapping = {
+	targetMuscleGroup: "타겟 부위",
+	exerciseType: "운동 유형",
+	equipmentRequired: "필요 장비",
+	mechanics: "기술",
+	forceType: "힘의 유형",
+	experienceLevel: "경험 수준",
+	secondaryMuscles: "보조 근육",
+};
+
 export function createExerciseProfile(exerciseInfo) {
-    var exerciseProfile = document.createElement("div");
-    exerciseProfile.id = "exercise-profile";
-    
-    var headline = document.createElement("h2");
-    headline.textContent = "운동 프로필";
-    exerciseProfile.appendChild(headline);
+	var exerciseProfile = document.createElement("div");
+	exerciseProfile.id = "exercise-profile";
 
-    var exerciseProfileTable = document.createElement("table");
-    exerciseProfileTable.id = "exercise-profile-table";
-    
-    for (var key in exerciseInfo) {
-        var tr = document.createElement("tr");
-        
-        var keyTd = document.createElement("td");
-        keyTd.textContent = key;
-        tr.appendChild(keyTd);
+	var headline = document.createElement("h2");
+	headline.textContent = "운동 프로필";
+	exerciseProfile.appendChild(headline);
 
-        var valueTd = document.createElement("td");
-        valueTd.textContent = exerciseInfo[key];
-        tr.appendChild(valueTd);
+	var exerciseProfileTable = document.createElement("table");
+	exerciseProfileTable.id = "exercise-profile-table";
 
-        exerciseProfileTable.appendChild(tr);
-    }
-        
-    exerciseProfile.appendChild(exerciseProfileTable);
-    return exerciseProfile;
+	for (var key in exerciseInfo) {
+		var tr = document.createElement("tr");
+
+		var keyTd = document.createElement("td");
+		keyTd.textContent = keyMapping[key];
+		console.log(keyMapping[key]);
+		tr.appendChild(keyTd);
+
+		var valueTd = document.createElement("td");
+		valueTd.textContent = exerciseInfo[key];
+		tr.appendChild(valueTd);
+
+		exerciseProfileTable.appendChild(tr);
+	}
+
+	exerciseProfile.appendChild(exerciseProfileTable);
+	return exerciseProfile;
 }
